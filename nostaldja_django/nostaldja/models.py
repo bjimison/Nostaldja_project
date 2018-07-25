140bec7d6718d5aa19268f3d996b279306ec9aa6
bjimison <brady.t.jimison@gmail.com>
2018-07-25 15:41:03 -0700

    project structure created

---
+++ nostaldja_django/nostaldja/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Decade(models.Model):
+  start_year = models.CharField(max_length=100)
+
+  def __str__(self):
+    return self.start_year
+
+class Fad(models.Model):
+  decade = models.ForeignKey(Decade, on_delete=models.CASCADE, related_name='fads')
+  name = models.CharField(max_length=100)
+  image_url = models.TextField(default='')
+  description = models.CharField(max_length=100)
+  decade = models.CharField(max_length=100)
+
+  def __str__(self):
+    return self.name
